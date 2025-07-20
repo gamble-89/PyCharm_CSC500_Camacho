68c164968ad438bb3ba7cc70a64d029da4a52c98
gamble-89 <chriscamacho7@gmail.com>
2025-07-20 08:28:23 -0600

    Adding coursework to repo

---
+++ Module1_Part1_Camacho.py
@@ -0,0 +1,17 @@
+# Write a Python program to find the addition and subtraction of two numbers.
+
+# Ask the user to input two numbers (num1 and num2). 
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Given those two numbers, add them together to find the output.
+addition = num1 + num2
+
+# Subtract the two numbers to find the output.
+subtraction = num1 - num2
+
+# Print the results for the user.
+print(f"The addition of {num1} and {num2} is: {addition}")
+print(f"The subtraction of {num1} and {num2} is: {subtraction}")
+
+# End of the program
---
+++ Module1_Part2_Camacho.py
@@ -0,0 +1,17 @@
+# Write a Python program to find the multiplication and division of two numbers.
+
+# Ask the user to input two numbers (num1 and num2). 
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Given those two numbers, multiply them together to find the output.
+product = num1 * num2
+
+# Divide the two numbers to find the output.
+quotient = num1 / num2
+
+# Print the results for the user.
+print(f"The multiplication of {num1} and {num2} is: {product}")
+print(f"The division of {num1} and {num2} is: {quotient}")
+
+# End of the program
