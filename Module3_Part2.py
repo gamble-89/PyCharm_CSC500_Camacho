c617f01fbef7bea308c0c6dbcefa49481af1a8d8
gamble-89 <chriscamacho7@gmail.com>
2025-08-02 15:42:35 -0600

    Adding coursework to repo

---
+++ Module3_Part2.py
@@ -0,0 +1,13 @@
+# This is a program to calculate what time it will be on a 24-hour alarm clock when it goes off
+# The program will prompt the user for the current time and how many hours they would like to wait
+# The program will then display the result
+
+# Ask the user for the time now (in hours)
+time_now = int(input("Enter the current time (in hours, 0-23): "))
+hours_to_wait = int(input("Enter the number of hours to wait for the alarm (in hours, 0-23): "))
+
+# Define total hours and calculate the future time
+total_hours = (time_now + hours_to_wait) % 24
+
+# Output the result on a 24-hour clock
+print(f"\nThe time when the alarm goes off will be: {total_hours}00")
